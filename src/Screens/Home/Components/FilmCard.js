4bcabe5fc728e9e676dd6b531af4a2adaa3a99b9
PierreMtz <pierremp10@gmail.com>
2021-05-31 14:42:30 +0200

    fin

--- src/Screens/Home/Components/FilmCard.js
+++ src/Screens/Home/Components/FilmCard.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faYoutube} from '@fortawesome/free-brands-svg-icons';
-import {faClock} from '@fortawesome/free-solid-svg-icons';
-import {Button} from 'react-bootstrap';
+
 
 class FilmCard extends React.Component {
   constructor(props){
     super(props)
   }
 
-  handleClick(){
-    this.props.showModal(this.props.film);
-  }
+ 
 
   render(){
     return (
@@ -20,8 +17,7 @@ class FilmCard extends React.Component {
         <div className="film-name">{this.props.film.film_Nom}</div>
         <div className="film-duree"> Durée : {this.props.film.film_Durée}</div>
         <div className="film-des">  {this.props.film.film_Description}</div>
-        <div onClick={() => this.handleClick()} className="button-film"><FontAwesomeIcon className="faIconColor" icon={faClock} /></div>
-        <div className="profile-icons"><a href="https://www.youtube.com/watch?v=Q1ioA_GnzeEab_channel=PeppaPigFran%C3%A7ais-Cha%C3%AEneOfficielle"  alt="Voir le trailer"><FontAwesomeIcon className="faIconColor" icon={faYoutube} /></a></div>
+        <div className="profile-icons"><a href="https://www.youtube.com"  alt="Voir le trailer"><FontAwesomeIcon className="faIconColor" icon={faYoutube} /></a></div>
       </div>
     );
   }
